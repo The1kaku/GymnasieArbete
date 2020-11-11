08fbc7c13ae00b8817501b3ddc5c5bf36f3e4fbe
The1kaku <54804810+The1kaku@users.noreply.github.com>
2020-11-11 13:37:46 +0100

    Updated Command Parser

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <curses.h>
 #include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
 #define COMMAND_SIZE 1000
+#define COMMAND_BUF_SIZE 100
 
 enum KEYS {
 	K_UP = 'w',
@@ -14,24 +16,47 @@ enum KEYS {
 };
 
 typedef struct {
+	char *str;
+	int (*fun)(int);
+	int time;
+} Command;
+
+
+typedef struct {
 	int health;
 	int symbol;
 	int y;
 	int x;
 } Actor;
 
+int EXIT_CODE = 0;
+
 Actor player;
 char command[COMMAND_SIZE];
+char *cmdptr = command;
 
 void displayActor(const Actor *a);
-void moveActor(Actor *a, int dy, int dx);
-int getInput();
-void parseCommand(const char *str);
+void moveActor(Actor *a, const int dy, const int dx);
+int getInput(void);
+char *sgetword(char *str);
+int parseCommand(const char *str);
+int exitCommand(int time);
+int moveCommand(int time);
 
-int main() 
+Command C_EXIT = { "exit", &exitCommand, 0 };
+Command C_MOVE = { "move", &moveCommand, 10 };
+
+Command activeCommands[COMMAND_BUF_SIZE];
+Command *end = &activeCommands[0];
+
+int main(void) 
 {
 	initscr();
-	raw();
+	cbreak();
+	noecho();
+
+	*end++ = C_EXIT;
+	*end++ = C_MOVE;
 
 	player.y = 0;
 	player.x = 0;
@@ -58,13 +83,13 @@ void displayActor(const Actor *a)
 	printw("Y:%d\tX:%d\nSYM:%c\nHP:%d\n", a->y, a->x, a->symbol, a->health);
 }
 
-void moveActor(Actor *a, int dy, int dx)
+void moveActor(Actor *a, const int dy, const int dx)
 {
 	a->y += dy;
 	a->x += dx;
 }
 
-int getInput()
+int getInput(void)
 {
 	int c = getch();
 	
@@ -82,11 +107,16 @@ int getInput()
 		moveActor(&player, 1, 0);
 		break;
 	case K_EXIT:
-		exit(0);
+		exitCommand(0);
 		break;
 	case K_COMMAND:
+		nocbreak();
+		echo();
+		addch('/');
 		getstr(command);
-		parseCommand(command);
+		parseCommand(sgetword(cmdptr = command));
+		noecho();
+		cbreak();
 		break;
 	default:
 		return -1;
@@ -94,8 +124,76 @@ int getInput()
 	return 0;
 }
 
-void parseCommand(const char *str)
+char *sgetword(char *str)
 {
-	if (strcmp(str, "exit") == 0)
-		exit(0);
+	char *begin;
+	char *end;
+	for (begin = str; isspace(*begin); ++begin)
+		;
+
+	for (end = begin; !isspace(*end); ++end)
+		;
+
+	*end = '\0';
+	
+	cmdptr = ++end;
+	return begin;
+}
+
+int parseCommand(const char *str)
+{
+	Command *begin;
+	for (begin = &activeCommands[0]; begin != end; begin++)
+		if (strcmp(str, begin->str) == 0) {
+			cbreak();
+			return (begin->fun)(begin->time);
+		}
+	return -1;
+}
+
+int exitCommand(int time)
+{
+	endwin();
+	exit(EXIT_CODE);
+}
+
+int moveCommand(int time)
+{
+	int delta = 0;
+	int dir;
+	char *directions[4] = {
+		"left",
+		"right",
+		"up",
+		"down"
+	};
+
+	char *dirstr = sgetword(cmdptr);
+	for (dir = 0; dir < 4; dir++) {
+		if (strcmp(dirstr, directions[dir]) == 0) {
+			delta = 1;
+			break;
+		}
+	}
+
+	char *numstr = sgetword(cmdptr);
+	sscanf(numstr, "%d", &delta);
+
+	if (dir < 4 && delta != 0)
+		switch (dir) {
+		case 0:
+			moveActor(&player, 0, -delta);
+			break;
+		case 1:
+			moveActor(&player, 0, delta);
+			break;
+		case 2:
+			moveActor(&player, -delta, 0);
+			break;
+		case 3:
+			moveActor(&player, delta, 0);
+			break;
+		}
+
+	return delta * time;
 }
